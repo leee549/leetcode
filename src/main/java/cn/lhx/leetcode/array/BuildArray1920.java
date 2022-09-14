d8022a083844d17aefa52f6b59c6fa7eefcaae94
leee549 <saltlee@foxmail.com>
2022-09-14 14:16:33 +0800

    update：每日一题数组

---
+++ src/main/java/cn/lhx/leetcode/array/BuildArray1920.java
@@ -0,0 +1,33 @@
+package cn.lhx.leetcode.array;
+
+import java.util.Arrays;
+
+/**
+ * 1920. 基于排列构建数组
+ * 给你一个 从 0 开始的排列 nums（下标也从 0 开始）。请你构建一个 同样长度 的数组 ans ，其中，对于每个 i（0 <= i < nums.length），都满足 ans[i] = nums[nums[i]] 。返回构建好的数组 ans 。
+ * <p>
+ * 从 0 开始的排列 nums 是一个由 0 到 nums.length - 1（0 和 nums.length - 1 也包含在内）的不同整数组成的数组。
+ * <p>
+ * 来源：力扣（LeetCode）
+ * 链接：https://leetcode.cn/problems/build-array-from-permutation
+ *
+ * @author: lihaox
+ * @date: 2022/5/20 10:06 上午
+ */
+
+public class BuildArray1920 {
+
+    public static void main(String[] args) {
+        int[] nums = {0, 2, 1, 5, 3, 4};
+        System.out.println(Arrays.toString(buildArray(nums)));
+    }
+
+    public static int[] buildArray(int[] nums) {
+        int[] res = new int[nums.length];
+        for (int i : nums) {
+            res[i] = nums[nums[i]];
+        }
+        return res;
+    }
+
+}
---
+++ src/main/java/cn/lhx/leetcode/array/GetConcatenation1929.java
@@ -0,0 +1,40 @@
+package cn.lhx.leetcode.array;
+
+import java.util.Arrays;
+
+/**
+ * 1929. 数组串联
+ * 给你一个长度为 n 的整数数组 nums 。请你构建一个长度为 2n 的答案数组 ans ，数组下标 从 0 开始计数 ，对于所有 0 <= i < n 的 i ，满足下述所有要求：
+ * ans[i] == nums[i]
+ * ans[i + n] == nums[i]
+ * 具体而言，ans 由两个 nums 数组 串联 形成。
+ * 返回数组 ans 。
+ * <p>
+ * 输入：nums = [1,2,1]
+ * 输出：[1,2,1,1,2,1]
+ * 解释：数组 ans 按下述方式形成：
+ * - ans = [nums[0],nums[1],nums[2],nums[0],nums[1],nums[2]]
+ * - ans = [1,2,1,1,2,1]
+ *
+ * @author: lihaox
+ * @date: 2022/5/20 10:06 上午
+ */
+
+public class GetConcatenation1929 {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3};
+        System.out.println(Arrays.toString(getConcatenation(nums)));
+    }
+
+    public static int[] getConcatenation(int[] nums) {
+        int[] res = new int[2 * nums.length];
+        for (int i = 0; i < nums.length; i++) {
+            res[i] = nums[i];
+            res[i + nums.length] = nums[i];
+        }
+        return res;
+    }
+
+
+}
